1d07b6a5d482366db31a01fc2fcbdf120c8bfbba
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2025-03-05 14:36:54 +0700

    server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/',(request,response)=>{
+    response.send(`
+        <!doctype html>
+        <html>
+        <head>
+            <title>Express.js</title>
+        </head>
+        <body>
+            <h3>Welcome to Express.js</h3>
+            <b>Express.js Fast, unipinionated, minimalist<br>
+            web framework for Node.js</br>
+        </body>
+        </html>
+    `)
+})
+app.listen(port,()=>console.log('Server listening on port'+port))
